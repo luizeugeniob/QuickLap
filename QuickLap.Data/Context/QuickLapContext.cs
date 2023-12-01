c36b09ee69d09e13567a148b929ef4459dcc5021
Luiz Eugênio Barbieri <luizeugeniobarbieri@gmail.com>
2023-12-01 15:30:38 +0100

    Adds customer table

--- QuickLap.Data/Context/QuickLapContext.cs
+++ QuickLap.Data/Context/QuickLapContext.cs
@@ -10,10 +10,12 @@ public class QuickLapContext : DbContext
     {
     }
 
+    public DbSet<Customer> Customers { get; set; }
     public DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.ApplyConfiguration(new CustomerConfiguration());
         modelBuilder.ApplyConfiguration(new UserConfiguration());
     }
 }
---
+++ QuickLap.Data/Entities/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace QuickLap.Data.Entities.Configurations;
+
+public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        builder
+            .ToTable("Customers")
+            .HasKey(u => u.Id);
+
+        builder
+            .HasIndex(u => u.Email)
+            .IsUnique();
+
+        builder
+            .Property(u => u.FirstName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder
+            .Property(u => u.LastName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder
+            .Property(u => u.Email)
+            .IsRequired()
+            .HasMaxLength(100);
+    }
+}
