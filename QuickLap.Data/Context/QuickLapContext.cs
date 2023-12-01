9a36036da06b6fc0d3cdce1aab7f071ea9f338cc
Luiz Eugênio Barbieri <luizeugeniobarbieri@gmail.com>
2023-12-01 15:35:37 +0100

    Adds product table

--- QuickLap.Data/Context/QuickLapContext.cs
+++ QuickLap.Data/Context/QuickLapContext.cs
@@ -11,11 +11,13 @@ public class QuickLapContext : DbContext
     }
 
     public DbSet<Customer> Customers { get; set; }
+    public DbSet<Product> Products { get; set; }
     public DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfiguration(new CustomerConfiguration());
+        modelBuilder.ApplyConfiguration(new ProductConfiguration());
         modelBuilder.ApplyConfiguration(new UserConfiguration());
     }
 }
---
+++ QuickLap.Data/Entities/Configurations/ProductConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace QuickLap.Data.Entities.Configurations;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder
+            .ToTable("Products")
+            .HasKey(u => u.Id);
+
+        builder
+            .Property(u => u.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder
+            .Property(u => u.Price)
+            .IsRequired();
+    }
+}
