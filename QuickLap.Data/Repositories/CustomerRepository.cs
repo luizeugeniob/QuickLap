ab4da4d933654d5b60b3b1d059637a961c028072
Luiz Eugênio Barbieri <luizeugeniobarbieri@gmail.com>
2023-12-05 17:09:09 +0100

    Adds customer repository

---
+++ QuickLap.Data/Repositories/CustomerRepository.cs
@@ -0,0 +1,23 @@
+﻿using QuickLap.Data.Context;
+using QuickLap.Data.Entities;
+using QuickLap.Data.Repositories.Internal;
+
+namespace QuickLap.Data.Repositories;
+
+public class CustomerRepository : ICustomerRepository
+{
+    private readonly QuickLapContext _context;
+
+    public CustomerRepository(QuickLapContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> RegisterCustomerAsync(string firstName, string lastName, string email, CancellationToken cancellation = default)
+    {
+        var customer = new Customer { FirstName = firstName, LastName = lastName, Email = email };
+        await _context.Customers.AddAsync(customer, cancellation);
+        await _context.SaveChangesAsync(cancellation);
+        return customer.Id;
+    }
+}
---
+++ QuickLap.Data/Repositories/Internal/ICustomerRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace QuickLap.Data.Repositories.Internal;
+
+internal interface ICustomerRepository
+{
+    Task<int> RegisterCustomerAsync(string firstName, string lastName, string email, CancellationToken cancellation = default);
+}
