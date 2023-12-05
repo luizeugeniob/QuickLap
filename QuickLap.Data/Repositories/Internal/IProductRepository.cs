65a1c9893d8a8576b083de367d9fcd102b9de986
Luiz Eugênio Barbieri <luizeugeniobarbieri@gmail.com>
2023-12-05 14:43:03 +0100

    Adds product repository

---
+++ QuickLap.Data/Repositories/Internal/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using QuickLap.Data.Entities;
+
+namespace QuickLap.Data.Repositories.Internal;
+
+internal interface IProductRepository
+{
+    Task<int> AddProductAsync(string name, decimal price, CancellationToken cancellation = default);
+    Task<List<Product>> GetAllAsync(CancellationToken cancellation = default);
+    Task<Product> GetByIdAsync(int id, CancellationToken cancellation = default);
+}
---
+++ QuickLap.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickLap.Data.Context;
+using QuickLap.Data.Entities;
+using QuickLap.Data.Exceptions;
+using QuickLap.Data.Repositories.Internal;
+
+namespace QuickLap.Data.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly QuickLapContext _context;
+
+    public ProductRepository(QuickLapContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> AddProductAsync(string name, decimal price, CancellationToken cancellation = default)
+    {
+        var product = new Product { Name = name, Price = price };
+        await _context.Products.AddAsync(product, cancellation);
+        await _context.SaveChangesAsync(cancellation);
+        return product.Id;
+    }
+
+    public async Task<List<Product>> GetAllAsync(CancellationToken cancellation = default)
+        => await _context.Products.ToListAsync(cancellation);
+
+    public async Task<Product> GetByIdAsync(int id, CancellationToken cancellation = default)
+        => await _context.Products.FirstOrDefaultAsync(p => p.Id.Equals(id), cancellation) ?? throw new NotFoundException<Product>(id);
+}
